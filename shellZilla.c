eb8588266e08d773f277a71e07e160a26c61555d
Yumingz <yzhang@cs.dal.ca>
2014-11-26 00:06:47 -0400

    Add main function. fixed #1.
    Add main function. fixed #1.

--- shellZilla.c
+++ shellZilla.c
@@ -13,3 +13,73 @@
 #define SHELL_HIS_CMD_CNT_MAX 10
 #define PATH_LEN_MAX 1024
 #define PROMPT_LEN_MAX 1024
+
+
+int main()
+{
+    char shellPrompt[PROMPT_LEN_MAX];
+    char cwd[PATH_LEN_MAX];
+    char *userInput = NULL;
+    char curFolder[64];
+    char *inputWithNoSpace = NULL;
+    char cmd[PROMPT_LEN_MAX];
+    char param[PROMPT_LEN_MAX];
+
+    /*init the completion function*/
+    funShellZillaReadLineIni();
+
+    /*set the command history count to 10*/
+    stifle_history(SHELL_HIS_CMD_CNT_MAX);
+    while(1)
+    {
+        memset(cwd, 0, sizeof(cwd));
+        getcwd(cwd, sizeof(cwd));
+
+        memset(curFolder, 0, sizeof(curFolder));
+        /*just get the folder name in current directory*/
+        funShellZillaGetCurFolder(cwd, curFolder);
+        memset(shellPrompt, 0, sizeof(shellPrompt));
+        
+        sprintf(shellPrompt, "%s@%s:%s $", getenv("USER"), "ShellZilla", curFolder);
+
+        /*read from stdinput*/
+        userInput = readline(shellPrompt);
+        if(NULL == userInput)
+        {
+            break;
+        }
+
+        /*get rid of the white space at the beginning and the end*/
+        inputWithNoSpace = funShellZillaGetRidOfSpace(userInput);
+        /*printf("%s\n", inputWithNoSpace);*/
+
+        /*add the command to history*/
+        add_history(inputWithNoSpace);
+        if(!funShellZillaIsValidCmd(inputWithNoSpace))
+        {
+            /*if command not supported,print warning info*/
+            printf("%s :command not found!\n", inputWithNoSpace);
+            free(userInput);
+            userInput = NULL;
+            continue;
+        }
+
+        memset(cmd, 0, PROMPT_LEN_MAX);
+        memset(param, 0, PROMPT_LEN_MAX);
+        /*parse the input,get the command name and the parameter*/
+        funShellZillaParse(inputWithNoSpace, cmd, param);
+        if(!strcmp(cmd, "quit"))
+        {
+            free(userInput);
+            userInput = NULL;
+            break;
+        }
+        /*excute the command*/
+        funShellZillaExc(cmd, param);
+
+        free(userInput);
+        userInput = NULL;
+    }
+    return OK;
+}
+#endif
